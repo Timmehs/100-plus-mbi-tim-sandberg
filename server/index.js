7186863fb6b795a6d50c62b49485a4d85aa69ab4
Tim Sandberg <tasandberg@gmail.com>
2022-02-11 15:56:10 -0800

    Fix regex

--- server/index.js
+++ server/index.js
@@ -7,18 +7,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+// Middleware
 app.use(morgan("tiny"))
 app.use(express.json())
 
-app.get("/generate", (req, res) => {
-  res.json({ mbi: generateMBI() })
-})
+// Routes
+app.get("/generate", (_req, res) => res.json({ mbi: generateMBI() }))
 
-app.post("/verify", (req, res) => {
-  const { mbi } = req.body
-  console.log(mbi, "mbi")
+app.post("/verify", (req, res) =>
   res.json({ isValid: validateMBI(req.body.mbi) })
-})
+)
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
--- server/util/validate-mbi.js
+++ server/util/validate-mbi.js
@@ -1,6 +1,6 @@
 // Validate MBI is in the form described: https://www.cms.gov/medicare/new-medicare-card/understanding-the-mbi-with-format.pdf
 
-const regex = /^[1-9][A-Z][0-9|A-Z][0-9][A-Z][0-9|A-Z][0-9][A-Z]{2}[0-9]{2}/g
+const regex = /^[1-9][A-Z][0-9|A-Z][0-9][A-Z][0-9|A-Z][0-9][A-Z]{2}[0-9]{2}$/
 
 export default function validateMBI(code) {
   return regex.test(code)
